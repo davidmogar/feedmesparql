d8413d530211c6a276cdbc6138e581a0ce1cf54b
David Moreno García <david.mogar@gmail.com>
2015-05-16 17:36:23 +0200

    Add a method to perform SPARQL requests
    It's unfinished yet. The idea is to receive filter options in a POST
    request, create a SPARQL query using those values, execute it and
    return Fuseki's answer.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var request = require('request');
+var util = require('util');
+
 var router = express.Router();
+
+const SPARQL_ENDPOINT = 'http://156.35.95.69:3030/ds/query';
+var SPARQL_REQUEST = '?query=SELECT * WHERE { %s }';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,6 +13,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/filter', function(req, res, next) {
+  var query = '?sub ?pred ?obj .';
+
+  var options = {
+    url: SPARQL_ENDPOINT + util.format(SPARQL_REQUEST, query),
+    headers : {
+      'Content-type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/sparql-results+json'
+    }
+  }
+
+  request(options, function(error, response, body) {
+    if (!error) {
+      if (response.statusCode == 200) {
+        res.send(body);
+      } else {
+        res.status(404).json(response)
+      }
+    } else {
+      res.render('503');
+    }
+  });
 });
 
 module.exports = router;
